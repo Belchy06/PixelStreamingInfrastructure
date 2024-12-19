f8f19edd31f35c7709b6631c906bf3f1d45a2cbe
Will Belcher <william.belcher@xa.epicgames.com>
2024-12-19 11:34:42 +1000

    Update player registry to do the identify->endpointId->endpointIdConfirm flow

--- Signalling/src/PlayerRegistry.ts
+++ Signalling/src/PlayerRegistry.ts
@@ -1,4 +1,4 @@
-import { SignallingProtocol, BaseMessage, EventEmitter } from '@epicgames-ps/lib-pixelstreamingcommon-ue5.5';
+import { SignallingProtocol, BaseMessage, EventEmitter, Messages, MessageHelpers } from '@epicgames-ps/lib-pixelstreamingcommon-ue5.5';
 import { Logger } from './Logger';
 import { IMessageLogger } from './LoggingUtils';
 import { IStreamer } from './StreamerRegistry';
@@ -35,14 +35,12 @@ export interface IPlayerInfo {
  *   'removed': (playerId: string) Player was removed.
  */
 export class PlayerRegistry extends EventEmitter {
-    private players: Map<string, IPlayer> = new Map();
-    private playerCount: number;
+    private players: IPlayer[];
     private nextPlayerId: number;
 
     constructor() {
         super();
-        this.players = new Map();
-        this.playerCount = 0;
+        this.players = [];
         this.nextPlayerId = 0;
     }
 
@@ -50,10 +48,15 @@ export class PlayerRegistry extends EventEmitter {
      * Assigns a unique id to the player and adds it to the registry
      */
     add(player: IPlayer): void {
-        player.playerId = this.getUniquePlayerId();
-        this.players.set(player.playerId, player);
-        this.playerCount++;
+        player.playerId = this.sanitizePlayerId(player.playerId); 
+
+        this.players.push(player);;
+
+        player.protocol.on(Messages.endpointId.typeName, this.onEndpointId.bind(this, player));
+        player.sendMessage(MessageHelpers.createMessage(Messages.identify));
+
         this.emit('added', player.playerId);
+        
         Logger.info(`Registered new player: ${player.playerId}`);
     }
 
@@ -62,22 +65,23 @@ export class PlayerRegistry extends EventEmitter {
      * does not exist.
      */
     remove(player: IPlayer): void {
-        if (!this.players.has(player.playerId)) {
+        const index = this.players.indexOf(player);
+        if (index == -1)
+        {
             return;
         }
 
+        this.players.splice(index, 1);
         this.emit('removed', player.playerId);
-        this.players.delete(player.playerId);
-        this.playerCount--;
 
         Logger.info(`Unregistered player: ${player.playerId}`);
     }
 
     /**
-     * Tests if a player id exists in the registry.
-     */
+    * Tests if a player id exists in the registry.
+    */
     has(playerId: string): boolean {
-        return this.players.has(playerId);
+        return this.get(playerId) !== undefined;
     }
 
     /**
@@ -85,30 +89,76 @@ export class PlayerRegistry extends EventEmitter {
      * Returns undefined if the player doesn't exist.
      */
     get(playerId: string): IPlayer | undefined {
-        return this.players.get(playerId);
+        return this.players.find((player) => player.playerId == playerId);
     }
 
     listPlayers(): IPlayer[] {
-        return Array.from(this.players.values());
+        return this.players;
     }
 
     /**
      * Returns true when the registry is empty.
      */
     empty(): boolean {
-        return this.players.size == 0;
+        return this.players.length == 0;
     }
 
     /**
      * Gets the total number of connected players.
      */
     count(): number {
-        return this.players.size;
+        return this.players.length;
     }
 
     private getUniquePlayerId(): string {
         const newPlayerId = `Player${this.nextPlayerId}`;
         this.nextPlayerId++;
         return newPlayerId;
+    }
+
+    private onEndpointId(player: IPlayer, message: Messages.endpointId): void {
+        const oldId = player.playerId;
+
+        if(!message.id)
+        {
+            message.id = oldId; // If the frontend doesn't specify a custom id, just the default playerId
+        }
+    
+        // id might conflict or be invalid so here we sanitize it
+        player.playerId = this.sanitizePlayerId(message.id);
+    
+        Logger.debug(`PlayerRegistry: Player id change. ${oldId} -> ${player.playerId}`);
+    
+        // because we might have sanitized the id, we confirm the id back to the player
+        player.sendMessage(
+            MessageHelpers.createMessage(Messages.endpointIdConfirm, { committedId: player.playerId })
+        );
+    }
+
+    private sanitizePlayerId(id: string): string {
+        // create a default id if none supplied
+        if (!id) {
+            id = this.getUniquePlayerId();
+        }
+
+        // search for existing playerId and optionally append a numeric value
+        let maxPostfix = -1;
+        for (const player of this.players) {
+            const idMatchRegex = /^(.*?)(\d*)$/;
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+            const [, baseId, postfix] = player.playerId.match(idMatchRegex)!;
+            // if the id is numeric then base id will be empty and we need to compare with the postfix
+            if ((baseId != '' && baseId != id) || (baseId == '' && postfix != id)) {
+                continue;
+            }
+            const numPostfix = Number(postfix);
+            if (numPostfix > maxPostfix) {
+                maxPostfix = numPostfix;
+            }
+        }
+        if (maxPostfix >= 0) {
+            return id + (maxPostfix + 1);
+        }
+        return id;
     }
 }
